04fb270bbd5d56be6d765d01ce8d923dcbddc3ef
Milan Pavlovic <pavlovicmilan5000@gmail.com>
2021-01-26 15:37:55 -0500

    movie 2 branch watching a movie for 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("B21 future SDET");
         //watching more movie in the bedroom
+        // movie on the phone 10 min
 
     }
 
