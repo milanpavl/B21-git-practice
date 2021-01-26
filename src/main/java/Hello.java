19ef119eb98a1954a4517be2f49cef3dcce92d4c
Milan Pavlovic <pavlovicmilan5000@gmail.com>
2021-01-26 15:27:28 -0500

    watching movie at the new location 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("B21 future SDET");
-
+        //watching more movie in the bedroom
+//watching tv
     }
 
 }
